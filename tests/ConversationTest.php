941bc2e1f6b734958a5f1ea00e219a223c53f80f
recca0120 <recca0120@gmail.com>
2023-09-06 08:11:30 +0800

    test list conversation by user

--- tests/ConversationTest.php
+++ tests/ConversationTest.php
@@ -2,6 +2,7 @@
 
 namespace Recca0120\CometChat\Tests;
 
+use JsonException;
 use Psr\Http\Client\ClientExceptionInterface;
 use Recca0120\CometChat\Conversation;
 use VCR\VCR;
@@ -19,46 +20,80 @@ class ConversationTest extends TestCase
 
     /**
      * @throws ClientExceptionInterface
-     * @throws \JsonException
+     * @throws JsonException
      */
     public function test_list_conversations(): void
     {
         VCR::insertCassette('list_conversation.yaml');
 
-//        $user = $this->client->user();
-//
-//        foreach ($user->all() as $record) {
-//            $user->delete($record['uid'], permanent: true);
-//        }
-//
-//        for ($i = 0; $i < 25; $i++) {
-//            try {
-//                $uid = 'conversation_'.str_pad((string) $i, 3, '0', STR_PAD_LEFT);
-//                $user->create(uid: $uid, name: $uid, withAuthToken: true);
-//                usleep(500);
-//            } catch (\Exception $e) {
-//                dump($e->getMessage());
-//                exit;
-//            }
-//        }
+//        $this->givenUsers();
+//        $this->givenMessages();
+        $result = iterator_to_array($this->conversation->all(unread: true, perPage: 5));
 
-//        $message = $this->client->message();
-//        foreach ($message->all() as $record) {
-//            $message->delete($record['id'], permanent: true);
-//        }
-//
-//        for ($i = 1; $i < 25; $i++) {
-//            try {
-//                $uid = 'conversation_'.str_pad((string) $i, 3, '0', STR_PAD_LEFT);
-//                $message->send(receiver: $uid, data: ['text' => 'Hi Tom!'], onBehalfOf: 'conversation_001');
-//                usleep(500);
-//            } catch (\Exception $e) {
-//                dump($e->getMessage());
-//            }
-//        }
+        self::assertCount(24, $result);
+    }
 
-        $generator = $this->conversation->all(unread: true, perPage: 5, onBehalfOf: 'conversation_001');
+    /**
+     * @throws ClientExceptionInterface
+     * @throws JsonException
+     */
+    public function test_list_conversations_by_user(): void
+    {
+        VCR::insertCassette('list_conversation_by_user.yaml');
 
-        self::assertCount(1, iterator_to_array($generator));
+//        $this->givenUsers();
+//        $this->givenMessages();
+
+        $result = iterator_to_array($this->conversation->all(unread: true, perPage: 5, onBehalfOf: 'conversation_001'));
+
+        self::assertCount(1, $result);
+    }
+
+    /**
+     * @return void
+     * @throws ClientExceptionInterface
+     * @throws JsonException
+     */
+    private function givenUsers(): void
+    {
+        $user = $this->client->user();
+
+        foreach ($user->all() as $record) {
+            $user->delete($record['uid'], permanent: true);
+        }
+
+        for ($i = 0; $i < 25; $i++) {
+            try {
+                $uid = 'conversation_'.str_pad((string) $i, 3, '0', STR_PAD_LEFT);
+                $user->create(uid: $uid, name: $uid, withAuthToken: true);
+                usleep(500);
+            } catch (\Exception $e) {
+                dump($e->getMessage());
+                exit;
+            }
+        }
+    }
+
+    /**
+     * @return void
+     * @throws ClientExceptionInterface
+     * @throws JsonException
+     */
+    private function givenMessages(): void
+    {
+        $message = $this->client->message();
+        foreach ($message->all() as $record) {
+            $message->delete($record['id'], permanent: true);
+        }
+
+        for ($i = 1; $i < 25; $i++) {
+            try {
+                $uid = 'conversation_'.str_pad((string) $i, 3, '0', STR_PAD_LEFT);
+                $message->send(receiver: $uid, data: ['text' => 'Hi Tom!'], onBehalfOf: 'conversation_001');
+                usleep(500);
+            } catch (\Exception $e) {
+                dump($e->getMessage());
+            }
+        }
     }
 }
