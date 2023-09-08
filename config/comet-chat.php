1224e4e570bd250ef1945ca63ab11cf94ce3195d
recca0120 <recca0120@gmail.com>
2023-09-08 18:02:13 +0800

    VITE_

--- config/comet-chat.php
+++ config/comet-chat.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_id' => env('COMET_CHAT_APP_ID'),
-    'region' => env('COMET_CHAT_REGION', 'us'),
-    'api_key' => env('COMET_CHAT_API_KEY'),
+    'app_id' => env('VITE_COMET_CHAT_APP_ID'),
+    'region' => env('VITE_COMET_CHAT_REGION', 'us'),
+    'api_key' => env('VITE_COMET_CHAT_API_KEY'),
 ];
