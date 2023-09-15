e635188482681766a34b13133e7243c004858025
recca0120 <recca0120@gmail.com>
2023-09-15 16:32:35 +0800

    bind BlockUser

--- src/CometChatServiceProvider.php
+++ src/CometChatServiceProvider.php
@@ -5,6 +5,7 @@ namespace Recca0120\CometChat;
 use GuzzleHttp\Client as GuzzleClient;
 use Illuminate\Support\ServiceProvider;
 use Recca0120\CometChat\Api\AuthToken;
+use Recca0120\CometChat\Api\BlockUser;
 use Recca0120\CometChat\Api\Conversation;
 use Recca0120\CometChat\Api\Message;
 use Recca0120\CometChat\Api\User;
@@ -15,16 +16,17 @@ class CometChatServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/cometchat.php', 'cometchat');
 
-        $this->app->singleton(Client::class, function () {
+        $this->app->bind(Client::class, function () {
             $config = config('cometchat');
 
             return new Client($config['app_id'], $config['api_key'], $config['region'], new GuzzleClient());
         });
 
-        $this->app->singleton(User::class, fn() => $this->app->make(Client::class)->user());
-        $this->app->singleton(AuthToken::class, fn() => $this->app->make(Client::class)->authToken());
-        $this->app->singleton(Message::class, fn() => $this->app->make(Client::class)->message());
-        $this->app->singleton(Conversation::class, fn() => $this->app->make(Client::class)->conversation());
+        $this->app->bind(User::class, fn() => $this->app->make(Client::class)->user());
+        $this->app->bind(AuthToken::class, fn() => $this->app->make(Client::class)->authToken());
+        $this->app->bind(Message::class, fn() => $this->app->make(Client::class)->message());
+        $this->app->bind(Conversation::class, fn() => $this->app->make(Client::class)->conversation());
+        $this->app->bind(BlockUser::class, fn() => $this->app->make(Client::class)->blockUser());
     }
 
     public function boot()
