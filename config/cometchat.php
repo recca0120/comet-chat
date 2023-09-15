cf3830cf9585f85aaf7c19f8a2fd84b74da7eaf9
recca0120 <recca0120@gmail.com>
2023-09-15 16:38:54 +0800

    auth key

--- config/cometchat.php
+++ config/cometchat.php
@@ -3,5 +3,6 @@
 return [
     'app_id' => env('VITE_COMETCHAT_APP_ID'),
     'region' => env('VITE_COMETCHAT_REGION', 'us'),
+    'auth_key' => env('VITE_COMETCHAT_AUTH_KEY'),
     'api_key' => env('VITE_COMETCHAT_API_KEY'),
 ];
